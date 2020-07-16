a917f31bc1aa87343a618077dfe9115de022320e
KihatK <skmbkang@gmail.com>
2020-07-16 18:38:02 +0900

    remove webpack compress

--- front/next.config.js
+++ front/next.config.js
@@ -1,24 +1,19 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
-const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = withBundleAnalyzer({
-    distDir: '.next',
+    compress: true,
     webpack(config, { webpack }) {
         const prod = process.env.NODE_ENV === 'production';
-        const plugins = [
-            ...config.plugins,
-            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
-        ];
-        if (prod) {
-            plugins.push(new CompressionPlugin()); // main.js.gz
-        }
         return {
             ...config,
             mode: prod ? 'production' : 'development',
             devtool: prod ? 'hidden-source-map' : 'eval',
-            plugins,
+            plugins: [
+                ...config.plugins,
+                new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+            ],
         };
     },
 });
